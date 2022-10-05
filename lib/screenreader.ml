5f92ad7fce8fa8de2e284d4c7323c3262e331f9d
FedericoAureliano <fmorarocha@gmail.com>
2022-10-05 11:48:51 -0700

    make interactive and fix bugs

--- lib/screenreader.ml
+++ lib/screenreader.ml
@@ -11,19 +11,20 @@ let get_relative_direction d = match d with
 
 type path = directions list
 
-let rec get_coordinates_helper (p: path): int * int = 
+let rec get_coordinates (p: path): int * int = 
   match p with
   | [] -> (0, 0)
-  | Left::ps -> begin match get_coordinates_helper ps with 
+  | Left::ps -> begin match get_coordinates ps with
+      | (0, _) -> (0, 0)
       | (x, 0) -> (x - 1, 0) 
       | (x, _) -> (x, 0)
     end
-  | Right::ps -> let (x, _) = get_coordinates_helper ps in (x + 1, 0)
-  | Down::ps -> let (x, y) = get_coordinates_helper ps in (x, y + 1)
-  | Up::ps -> let (x, y) = get_coordinates_helper ps in (x, y - 1)
-
-let get_coordinates (p: path): int * int = 
-  get_coordinates_helper (List.rev p)
+  | Right::ps -> let (x, _) = get_coordinates ps in (x + 1, 0)
+  | Down::ps -> let (x, y) = get_coordinates ps in (x, y + 1)
+  | Up::ps -> begin match get_coordinates ps with
+      | (x, 0) -> (x, 0)
+      | (x, y) -> (x, y - 1)
+    end
 
 let bfs(exp: s_exp) (num: int): s_exp option =
   let q : s_exp Queue.t = Queue.create () in
@@ -39,13 +40,13 @@ let bfs(exp: s_exp) (num: int): s_exp option =
 
 let read_expr e = 
   match e with
-  | Lst (Sym f::args) -> Printf.sprintf "%s expressions, %d inputs\n" f (List.length args) 
+  | Lst (Sym f::args) -> Printf.sprintf "%s function call with %d inputs\n" f (List.length args) 
   | Sym f -> f
   | Num n -> string_of_int n
   | _ -> "Malformed s-expression\n"
 
 let rec get_error (defns: defn list) (body: s_exp) (p: path) : string =
-  match List.rev p with
+  match p with
   | [] -> "Empty program\n"
   | x::xs -> Printf.sprintf "No expression %s of %s" (get_relative_direction x) (read_position defns body xs)
 
@@ -63,13 +64,28 @@ and read_position (defns: defn list) (body: s_exp) (p: path) : string =
     end
   | None when n = List.length defns && d = 0 -> Printf.sprintf "The body of the program\n"
   | None when n = List.length defns && d > 0 ->
-    begin match (bfs body d) with
+    begin match (bfs body (d - 1)) with
       | Some e -> Printf.sprintf "%s\n" (read_expr e)
       | _ -> get_error defns body p
     end
   | _ -> get_error defns body p
 
-let navigate (program : string) (p: path) : unit =
-  let defns, body = parse_many program |> defns_and_body in
+let naivgate_to (defns : defn list) (body: s_exp) (p: path) : unit =
   let text = read_position defns body p in
-  Sys.command (Printf.sprintf "say \"%s\"" text) |> ignore;
+  Sys.command (Printf.sprintf "say \"%s\"" text) |> ignore
+
+let navigate (program : string) : unit =
+  let defns, body = parse_many program |> defns_and_body in
+  let c = ref (read_int_opt ()) in
+  let p = ref [] in
+  while !c <> (Some 6) do
+    begin match !c with
+      | Some 1 -> (p := (Left::!p); naivgate_to defns body !p)
+      | Some 2 -> (p := (Up::!p); naivgate_to defns body !p)
+      | Some 3 -> (p := (Down::!p); naivgate_to defns body !p)
+      | Some 4 -> (p := (Right::!p); naivgate_to defns body !p)
+      | Some 5 -> naivgate_to defns body !p
+      | _ -> Sys.command ("say \"Unknown input\"") |> ignore
+    end
+  ; c := read_int_opt ();
+  done
